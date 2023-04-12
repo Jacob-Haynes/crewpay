7d6e219095894fe26237aa3dd89b3b06c5b81f60
Tom Clements <tclements@exscientia.co.uk>
2023-04-12 15:10:14 +0100

    style: black + isort

--- src/crewpay/urls.py
+++ src/crewpay/urls.py
@@ -15,17 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 from crewpay import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", views.root, name="root"),
-    path('about', views.about, name="about"),
-    path('contact', views.contact, name="contact"),
-    path('token', views.token, name="token"),
-    path('login', views.login_view, name="login"),
-    path('logout', views.logout_view, name="logout"),
-    path("api/v1", include('api.v1.urls')),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("token", views.token, name="token"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("api/v1", include("api.v1.urls")),
 ]
--- src/crewpay/views.py
+++ src/crewpay/views.py
@@ -1,11 +1,10 @@
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate, login, logout
-
-from django.conf import settings
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -17,12 +16,11 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
 def root(request):
     if not request.user.is_authenticated:
         failed = False
-        if 'login_failed' not in request.session:
-            request.session['login_failed'] = False
+        if "login_failed" not in request.session:
+            request.session["login_failed"] = False
         else:
             failed = True
-            request.session.pop('login_failed')
-
+            request.session.pop("login_failed")
 
         return render(request, "not_logged_in/root.html", {"is_login_failed": failed})
 
@@ -30,31 +28,31 @@ def root(request):
 
 
 def login_view(request):
-    user = authenticate(username=request.POST['username'], password=request.POST['password'])
+    user = authenticate(username=request.POST["username"], password=request.POST["password"])
     if user is None:
-        request.session['login_failed'] = True
-        return redirect('root')
+        request.session["login_failed"] = True
+        return redirect("root")
 
     login(request, user)
-    return redirect('root')
+    return redirect("root")
 
 
 def logout_view(request):
     logout(request)
-    return redirect('root')
+    return redirect("root")
 
 
-@login_required(login_url='/')
+@login_required(login_url="/")
 def about(request):
     return render(request, "logged_in/root.html")
 
 
-@login_required(login_url='/')
+@login_required(login_url="/")
 def contact(request):
     return render(request, "logged_in/root.html")
 
 
-@login_required(login_url='/')
+@login_required(login_url="/")
 def token(request):
     token = Token.objects.get(user__exact=request.user)
     return render(request, "logged_in/token.html", {"token": token})
