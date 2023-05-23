e70f75e5333c2d91e5bfe1331984ff48ad10a65f
Jacob Haynes <jacobhaynes49@gmail.com>
2023-05-23 16:41:49 +0100

    refactor: refactored payrun

--- src/api/v1/staffology/payruns.py
+++ src/api/v1/staffology/payruns.py
@@ -1,139 +1,18 @@
-import itertools
-import json
-from datetime import datetime as dt
-from datetime import timedelta
-from typing import Dict, Optional
-
-from dateutil.relativedelta import relativedelta
 from django.contrib.auth.decorators import user_passes_test
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from api.v1.crewplanner.dto_cp_ShiftsByUser import CPShiftsByUser
-from api.v1.crewplanner.report import create_shift_lines
-from api.v1.staffology.dto_so_pay_options_import import Line, PayLine
-from api.v1.staffology.employers import (
-    StaffologyEmployerAPI,
-    activate_payruns,
-    update_employer_db,
-)
-from crewpay.models import Employee, Employer, InvalidShift
+from api.v1.staffology.payroll_manager import PayrollManager
 
 
 @api_view(["GET"])
 @user_passes_test(lambda u: u.is_superuser)
-def run_payroll(request: Request):
+def run_payroll(request: Request) -> Response:
     """API endpoint"""
     employer = request.GET["employer"]
     tax_year = request.GET["tax_year"]
     arrears = int(request.GET["period_arrears"])
-    return payroll_function(employer, tax_year, arrears)
-
-
-def payroll_function(employer: str, tax_year: str, arrears: int):
-    """imports the earnings reports into a staffology payrun"""
-    # update employer options
-    update_employer_db(employer)
-    user = Employer.objects.get(id=employer).user
-    pay_period = Employer.objects.get(id=employer).pay_period
-    # get pay schedules
-    schedule_to_run = StaffologyEmployerAPI().get_pay_schedule(employer, tax_year, pay_period)
-    # activate/get the payrun
-    pay_run = activate_payruns(schedule_to_run, employer, tax_year, pay_period)
-    # get the full payrun to edit
-    pay_run = StaffologyEmployerAPI().get_pay_run(employer, tax_year, pay_period, pay_run["metadata"]["periodNumber"])
-    # get shift lines from CP
-    earnings_period_start, earnings_period_end = compute_report_dates(
-        pay_run["startDate"], pay_run["endDate"], pay_period, arrears
-    )
-    shift_lines, failed_shifts = create_shift_lines(user, earnings_period_start, earnings_period_end)
-    # group by employee
-    shifts_by_user = []
-    for user_id, shifts in itertools.groupby(shift_lines, key=lambda x: x.worker.user.id):
-        shifts_by_user.append(CPShiftsByUser(user_id=user_id, shifts=list(shifts)))
-    # convert to Staffology pay lines
-    pay_lines = []
-    for user_shifts in shifts_by_user:
-        pay_lines.append(create_pay_line(user_shifts))
-    # post to staffology api
-    payload = [pay_line.dict() for pay_line in pay_lines]
-    StaffologyEmployerAPI().import_pay(employer, pay_period, payload)
-    # totals
-    shifts_added = len(shift_lines)
-    users_added = len(pay_lines)
-    failures = InvalidShift.objects.filter(employer=employer).order_by("date_time")[:failed_shifts]
-    failures_list = []
-    for failure in failures:
-        failure_dict = {
-            "project": failure.project,
-            "employee": failure.employee,
-            "error": failure.error,
-            "date_time": failure.date_time,
-        }
-        failures_list.append(failure_dict)
-
-    return Response(
-        {
-            "shifts_added": shifts_added,
-            "users_added": users_added,
-            "Failed Imports": failures_list,
-        }
-    )
-
-
-def compute_report_dates(start_date: str, end_date: str, pay_period: str, arrears: int):
-    """computes the dates required for the earnings report based on pay period and arrears"""
-    payrun_start_date = dt.strptime(start_date, "%Y-%m-%d")
-    payrun_end_date = dt.strptime(end_date, "%Y-%m-%d")
-    if arrears == 0:
-        earnings_period_start = start_date
-        earnings_period_end = end_date
-    else:
-        if pay_period == "Monthly":
-            report_start_date = payrun_start_date + relativedelta(months=-arrears)
-            report_end_date = report_start_date + relativedelta(months=1, days=-1)
-            earnings_period_start = report_start_date.strftime("%Y-%m-%d")
-            earnings_period_end = report_end_date.strftime("%Y-%m-%d")
-        else:
-            delta = compute_timedelta(pay_period, arrears)
-            report_start_date = payrun_start_date - delta
-            report_end_date = payrun_end_date - delta
-            earnings_period_start = report_start_date.strftime("%Y-%m-%d")
-            earnings_period_end = report_end_date.strftime("%Y-%m-%d")
-    return earnings_period_start, earnings_period_end
-
-
-def compute_timedelta(pay_period: str, arrears: int) -> timedelta:
-    """computes time deltas for various arrears and pay periods"""
-    if pay_period == "Daily":
-        return timedelta(days=arrears)
-    elif pay_period == "Weekly":
-        return timedelta(weeks=arrears)
-    elif pay_period == "Fortnightly":
-        return timedelta(weeks=(2 * arrears))
-    elif pay_period == "Four Weekly":
-        return timedelta(weeks=(4 * arrears))
-    else:
-        raise ValueError("Invalid pay period specified.")
-
-
-def create_pay_line(user_shifts: CPShiftsByUser) -> PayLine:
-    """Converts  cp shifts by user to a staffology Payline"""
-    new_lines = []
-    for shift in user_shifts.shifts:
-        code = "BASICHOURLY" if shift.wage.type_ == "hourly" else "BASICDAILY"
-        new_lines.append(
-            Line(
-                value=shift.wage.total,
-                rate=shift.wage.value,
-                multiplier=shift.timesheet.registered.netto_minutes / 60,
-                description=f"{shift.project.date} - {shift.project.company.name} - {shift.project.name}",
-                code=code,
-            )
-        )
-
-    return PayLine(
-        payrollCode=Employee.objects.get(crewplanner_id=user_shifts.user_id).payroll_code,
-        lines=new_lines,
-    )
+    manager = PayrollManager(employer=employer, tax_year=tax_year, arrears=arrears)
+    result = manager.process_payroll()
+    return Response(result)
--- tests/test_report.py
+++ tests/test_report.py
@@ -1,39 +1,166 @@
+from unittest.mock import Mock, patch
+
 import pytest
 
-from api.v1.crewplanner.report import api_get_cp_report
-from api.v1.staffology.payruns import compute_report_dates, payroll_function
-from crewpay.models import CrewplannerUser, Employer
+from api.v1.crewplanner.dto_cp_ShiftsByUser import CPShiftsByUser
+from api.v1.staffology.date_handeling import compute_report_dates
+from api.v1.staffology.dto_so_pay_options_import import Line, PayLine
+from api.v1.staffology.pay_line import create_pay_line
 
 
-def test_report_get():
-    stub = "demo-4"
-    start_date = "2023-01-13"
-    end_date = "2023-04-13"
-    access_token = CrewplannerUser.objects.get(stub=stub).access_key
-    report = api_get_cp_report(stub, access_token, start_date, end_date)
-    return
-
-
-def test_run_payroll():
-    employer = Employer.objects.get(user__username="CP demo").id
-    tax_year = "Year2023"
-    payroll_function(employer, tax_year)
-    return
-
-
-def test_compute_report_dates():
+@patch("api.v1.staffology.payruns.Employee")
+def test_create_pay_line(mock_employee: Mock):
     # Arrange
-    payrun_start_date = "2023-04-01"
-    payrun_end_date = "2023-04-30"
-    pay_period = "Monthly"
-    arrears = 1
-    result_start = "2023-03-01"
-    result_end = "2023-03-31"
+    mock_employee.objects.get.return_value.payroll_code = "EMP123"
+    user_shifts = CPShiftsByUser(
+        user_id=123,
+        shifts=[
+            {
+                "id": 1,
+                "date": "2023-01-01",
+                "start_time": "09:00",
+                "end_time": "11:00",
+                "present": "true",
+                "slot": {
+                    "id": 1,
+                    "department": {
+                        "id": 1
+                    },
+                    "function": {
+                        "id": 1,
+                        "name": "Waiter"
+                    }
+                },
+                "wage": {
+                    "type": "hourly",
+                    "total": 10.0,
+                    "value": 15.0
+                },
+                "timesheet": {
+                    "planned": {
+                        "start": "09:00",
+                        "end": "11:00",
+                        "minutes": 120
+                    },
+                    "registered": {
+                        "start": "09:00",
+                        "end": "11:00",
+                        "gross_minutes": 120,
+                        "netto_minutes": 120
+                    }
+                },
+                "worker": {
+                    "id": 1,
+                    "name": "John Smith",
+                    "contract_type": "EMP",
+                    "user": {
+                        "id": 1,
+                        "first_name": "John",
+                        "last_name": "Smith",
+                        "email": "john@smith.com",
+                        "date_of_birth": "2000-01-01"
+                    }
+                },
+                "project": {
+                    "id": 1,
+                    "name": "Project A",
+                    "date": "2023-01-01",
+                    "company": {
+                        "id": 1,
+                        "name": "Company A"
+                    }
+                }
+            },
+            # Add more shifts if needed
+        ]
+    )
+
+    expected_payline = PayLine(
+        payrollCode="EMP123",
+        lines=[
+            Line(
+                value=10.0,
+                rate=15.0,
+                multiplier=2.0,
+                description="2023-01-01 - Company A - Project A",
+                code="BASICHOURLY"
+            )
+            # Add more expected lines if needed
+        ]
+    )
 
     # Act
-    earnings_period_start, earnings_period_end = compute_report_dates(payrun_start_date, payrun_end_date, pay_period, arrears)
+    actual_payline = create_pay_line(user_shifts)
 
+    # Assert
+    assert actual_payline == expected_payline
+
+    # Additional assertion for specific fields
+    assert actual_payline.payrollCode == "EMP123"
+    assert len(actual_payline.lines) == len(expected_payline.lines)
+
+
+@pytest.mark.parametrize("payrun_start_date, payrun_end_date, pay_period, arrears, expected_start, expected_end",
+                         [("2023-01-01", "2023-01-31", "Monthly", 1, "2022-12-01", "2022-12-31"),
+                          ("2023-01-01", "2023-01-31", "Monthly", 2, "2022-11-01", "2022-11-30"),
+                          ("2023-01-01", "2023-01-31", "Monthly", 3, "2022-10-01", "2022-10-31"),
+                          ("2023-04-01", "2023-04-30", "Monthly", 1, "2023-03-01", "2023-03-31"),
+                          ("2023-04-01", "2023-04-30", "Monthly", 2, "2023-02-01", "2023-02-28"),
+                          ("2023-04-01", "2023-04-30", "Monthly", 3, "2023-01-01", "2023-01-31"),
+                          ("2023-01-01", "2023-01-07", "Weekly", 1, "2022-12-25", "2022-12-31"),
+                          ("2023-01-01", "2023-01-14", "Fortnightly", 1, "2022-12-18", "2022-12-31"),
+                          ("2023-01-01", "2023-01-14", "Fortnightly", 2, "2022-12-04", "2022-12-17"),
+                          ("2023-01-01", "2023-01-02", "Daily", 1, "2022-12-31", "2023-01-01"),
+                          ("2023-01-01", "2023-01-02", "Daily", 2, "2022-12-30", "2022-12-31"),
+                          ("2023-01-01", "2023-01-02", "Daily", 3, "2022-12-29", "2022-12-30"),
+                          ("2024-02-01", "2024-02-29", "Monthly", 1, "2024-01-01", "2024-01-31"),
+                          ("2024-02-01", "2024-02-29", "Monthly", 2, "2023-12-01", "2023-12-31"),
+                          ("2024-02-01", "2024-02-29", "Monthly", 3, "2023-11-01", "2023-11-30"),
+                          ("2023-01-01", "2023-01-31", "Monthly", 0, "2023-01-01", "2023-01-31"),
+                          ("2023-04-01", "2023-04-30", "Monthly", 0, "2023-04-01", "2023-04-30"),
+                          ("2023-02-01", "2023-02-28", "Monthly", 0, "2023-02-01", "2023-02-28"),
+                          ("2023-01-01", "2023-01-07", "Weekly", 0, "2023-01-01", "2023-01-07"),
+                          ("2023-01-01", "2023-01-14", "Fortnightly", 0, "2023-01-01", "2023-01-14"),
+                          ("2023-01-01", "2023-01-02", "Daily", 0, "2023-01-01", "2023-01-02"),
+                          ("2024-02-01", "2024-02-29", "Monthly", 0, "2024-02-01", "2024-02-29"),
+                          ("2023-01-01", "2023-01-07", "Weekly", 2, "2022-12-18", "2022-12-24"),
+                          ("2023-01-01", "2023-01-07", "Weekly", 3, "2022-12-11", "2022-12-17"),
+                          ("2023-04-01", "2023-04-07", "Weekly", 2, "2023-03-18", "2023-03-24"),
+                          ("2023-04-01", "2023-04-07", "Weekly", 3, "2023-03-11", "2023-03-17"),
+                          ("2023-02-01", "2023-02-07", "Weekly", 2, "2023-01-18", "2023-01-24"),
+                          ],
+                         ids=["over year boundary with monthly period",
+                              "over year boundary 2 month arrears",
+                              "over year boundary 3 month arrears",
+                              "30th to 31st month end, 1 month arrears",
+                              "30th to feb 28th",
+                              "30th over feb 28th",
+                              "weekly, 1 arrears",
+                              "fortnightly, 1 arrears",
+                              "fortnightly, 2 arrears",
+                              "daily, 1 arrears",
+                              "daily, 2 arrears",
+                              "daily, 3 arrears",
+                              "leap year, 1 month arrears",
+                              "leap year, 2 month arrears",
+                              "leap year, 3 month arrears",
+                              "monthly, 0 arrears - case 1",
+                              "monthly, 0 arrears - case 2",
+                              "monthly, 0 arrears - case 3",
+                              "weekly, 0 arrears",
+                              "fortnightly, 0 arrears",
+                              "daily, 0 arrears",
+                              "leap year, 0 arrears",
+                              "weekly, 2 arrears - case 1",
+                              "weekly, 3 arrears - case 1",
+                              "weekly, 2 arrears - case 2",
+                              "weekly, 3 arrears - case 2",
+                              "weekly, 2 arrears - case 3",
+                              ])
+def test_compute_report_dates(payrun_start_date, payrun_end_date, pay_period, arrears, expected_start, expected_end):
+    # Act
+    earnings_period_start, earnings_period_end = compute_report_dates(payrun_start_date, payrun_end_date, pay_period,
+                                                                      arrears)
     # Asset
-    assert earnings_period_start == result_start
-    assert earnings_period_end == result_end
-
+    assert earnings_period_start == expected_start
+    assert earnings_period_end == expected_end
