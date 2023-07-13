aee4234480ac565dee78c88327f214ef33f0030b
Jacob Haynes <jacobhaynes49@gmail.com>
2023-07-13 17:45:06 +0100

    fix: cleanup of old code

--- src/api/v1/crewplanner/api/employee.py
+++ src/api/v1/crewplanner/api/employee.py
@@ -9,34 +9,3 @@ class CPEmployeeAPI(CrewPlannerAPI):
         endpoint = "/employees"
         params = {"filter[status]": "verified", "filter[contract_types][]": ["VSA", "EMP"], "filter[payrolling]": "no"}
         return self._get(endpoint, params=params)
-
-    # def get_employees(self) -> list[Optional[CPEmployee]]:
-    #     """Get and validate CrewPlanner employees."""
-    #     results = self._get(
-    #         "employees",
-    #         {"filter[status]": "verified", "filter[contract_types][]": ["VSA", "EMP"], "filter[payrolling]": "no"},
-    #     )
-    #     return [self.validate_employee(employee) for employee in results.json()]
-    #
-    # def validate_employee(self, employee: dict) -> Optional[CPEmployee]:
-    #     """Validates a CrewPlanner employee by checking for missing required fields. Saves the invalid employee creation
-    #     attempt to the DB with the reason for failure."""
-    #     try:
-    #         for field in ["payroll_employee_statement", "payroll_student_loan_plan", "payroll_postgrad_loan"]:
-    #             field_value = employee["custom_fields"].get(field, [])
-    #             if isinstance(field_value, list) and len(field_value) > 0:
-    #                 employee["custom_fields"][field] = field_value[0]
-    #             else:
-    #                 employee["custom_fields"][field] = None
-    #         cp_employee = CPEmployee(**employee)
-    #         return cp_employee
-    #     except ValidationError as e:
-    #         name = f"{employee['first_name']} {employee['last_name']}"
-    #         invalid_employee = InvalidEmployee(
-    #             employee_id=employee["id"],
-    #             name=name,
-    #             error=json.dumps(e.errors()),
-    #             employer=Employer.objects.get(user__crewplanner_user__stub=self.stub),
-    #         )
-    #         invalid_employee.save()
-    #         return None
